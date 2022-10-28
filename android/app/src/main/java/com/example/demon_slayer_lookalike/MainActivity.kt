a90e866da983158fc88c88c370465ff51290ac82
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2022-10-29 02:42:16 +0900

    [앱] main 페이지

--- android/app/src/main/java/com/example/demon_slayer_lookalike/MainActivity.kt
+++ android/app/src/main/java/com/example/demon_slayer_lookalike/MainActivity.kt
@@ -8,6 +8,7 @@ import android.media.ThumbnailUtils
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.demon_slayer_lookalike.databinding.ActivityMainBinding
 import com.example.demon_slayer_lookalike.ml.Model
@@ -19,19 +20,12 @@ import kotlin.math.min
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    val imageSize = 224
+    private val imageSize = 224
+    private var maxPos = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.takePicture.setOnClickListener {
-            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                startActivityForResult(cameraIntent, 1)
-            } else {
-                requestPermissions(arrayOf(Manifest.permission.CAMERA), 100)
-            }
-        }
     }
 
     private fun callAi(image: Bitmap) {
@@ -55,56 +49,7 @@ class MainActivity : AppCompatActivity() {
         val outputs = model.process(inputFeature0)
         val outputFeature0 = outputs.outputFeature0AsTensorBuffer
         val confidences = outputFeature0.floatArray
-        var maxPos = 0
-        var maxConfidence = 0f
-        for (i in confidences.indices) {
-            if(confidences[i] > maxConfidence) {
-                maxConfidence = confidences[i]
-                maxPos = i
-            }
-        }
-        val classes = arrayOf(
-            "카마도 탄지로",
-            "카마도 네즈코",
-            "아가츠마 젠이츠",
-            "하시바라 이노스케",
-            "렌고쿠 쿄쥬로",
-            "우즈이 텐겐",
-            "칸로지 미츠리",
-            "히메지마 교메이",
-            "토키토 무이치로",
-            "이구로 오바나이",
-            "토미오카 기유",
-            "코쵸우 시노부",
-            "시나즈가와 사네미",
-            "코쵸우 카나에",
-            "우로코다키 사콘지",
-            "쿠와지마 지고로",
-            "츠유리 카나오",
-            "시나즈가와 겐야",
-            "칸자키 아오이",
-            "카이가쿠",
-            "무라타",
-            "우부야시키 카가야",
-            "하가네즈카 호타루",
-            "키부츠지 무잔",
-            "스사마루",
-            "야하바",
-            "루이",
-            "코쿠시보",
-            "아카자",
-            "도우마",
-            "굣코",
-            "다키",
-            "한텐구",
-            "나키메",
-            "쿄우가이",
-            "엔무",
-            "타마요",
-            "유시로",
-            "규타로"
-        )
-        binding.name.text = classes[maxPos]
+        confidences.forEachIndexed { index, fl -> if (fl == confidences.maxOrNull()!!) maxPos = index }
         model.close()
     }
 
@@ -116,7 +61,29 @@ class MainActivity : AppCompatActivity() {
             binding.img.setImageBitmap(image)
             image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false)
             callAi(image)
+            binding.addPhoto.visibility = View.GONE
+            binding.rePhoto.visibility = View.VISIBLE
         }
         super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    fun picture(view: View) {
+        if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, 1)
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), 100)
+        }
+    }
+
+    fun result(view: View) {
+        val intent = Intent(this, ResultActivity::class.java)
+        intent.putExtra("maxPos", maxPos)
+        startActivity(intent)
+        finish()
+    }
+
+    fun back(view: View) {
+        finish()
     }
 }

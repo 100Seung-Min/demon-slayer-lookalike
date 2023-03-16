f08b6319a599e195f2e979c15f355692d1c1521e
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-03-16 17:03:23 +0900

    뷰 base 파일 생성

---
+++ android/app/src/main/java/com/example/demon_slayer_lookalike/view/base/BaseActivity.kt
@@ -0,0 +1,25 @@
+package com.example.demon_slayer_lookalike.view.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<B : ViewDataBinding>(
+    @LayoutRes private val layoutId: Int,
+) : AppCompatActivity() {
+
+    protected lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        binding.lifecycleOwner = this
+
+        createView()
+    }
+
+    abstract fun createView()
+}

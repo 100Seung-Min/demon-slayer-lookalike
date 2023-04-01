5db400a646d277b60d5ab14ac8c99a33925ad643
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-04-01 23:27:10 +0900

    짱구 캐릭터 정리

---
+++ android/app/src/main/java/com/example/demon_slayer_lookalike/character_list/ShinChan.kt
@@ -0,0 +1,27 @@
+package com.example.demon_slayer_lookalike.character_list
+
+import com.example.demon_slayer_lookalike.R
+
+val shinChan = arrayOf(
+    Character("짱구", R.id.demonSlayerBtn),
+    Character("신형만", R.id.demonSlayerBtn),
+    Character("봉미선", R.id.demonSlayerBtn),
+    Character("신짱아", R.id.demonSlayerBtn),
+    Character("흰둥이", R.id.demonSlayerBtn),
+    Character("김철수", R.id.demonSlayerBtn),
+    Character("한유리", R.id.demonSlayerBtn),
+    Character("이훈이", R.id.demonSlayerBtn),
+    Character("맹구", R.id.demonSlayerBtn),
+    Character("한수지", R.id.demonSlayerBtn),
+    Character("치타", R.id.demonSlayerBtn),
+    Character("원장선생님", R.id.demonSlayerBtn),
+    Character("채성아", R.id.demonSlayerBtn),
+    Character("나미리", R.id.demonSlayerBtn),
+    Character("차은주", R.id.demonSlayerBtn),
+    Character("철수엄마", R.id.demonSlayerBtn),
+    Character("유리엄마", R.id.demonSlayerBtn),
+    Character("훈이엄마", R.id.demonSlayerBtn),
+    Character("유이슬", R.id.demonSlayerBtn),
+    Character("박광자", R.id.demonSlayerBtn),
+    Character("액션가면", R.id.demonSlayerBtn),
+)

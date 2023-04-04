ddf14345f811d36bdc07d4f05a5e365c8361d613
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-04-04 10:46:16 +0900

    귀멸의 칼날 이미지 url로 변경

--- android/app/src/main/java/com/example/demon_slayer_lookalike/character_list/demonSlayer.kt
+++ android/app/src/main/java/com/example/demon_slayer_lookalike/character_list/demonSlayer.kt
@@ -3,43 +3,133 @@ package com.example.demon_slayer_lookalike.character_list
 import com.example.demon_slayer_lookalike.R
 
 val demonSlayer = arrayOf(
-    Character("카마도 탄지로", R.raw.tanjiro),
-    Character("카마도 네즈코", R.drawable.nezuko),
-    Character("아가츠마 젠이츠", R.raw.zenitsu),
-    Character("하시바라 이노스케", R.raw.inosuke),
-    Character("렌고쿠 쿄쥬로", R.raw.rengoku),
-    Character("우즈이 텐겐", R.raw.tengen),
-    Character("칸로지 미츠리", R.raw.mitsuri),
-    Character("히메지마 교메이", R.raw.gyomei),
-    Character("토키토 무이치로", R.raw.muichiro),
-    Character("이구로 오바나이", R.raw.obanai),
-    Character("토미오카 기유", R.raw.giyu),
-    Character("코쵸우 시노부", R.raw.shinobu),
-    Character("시나즈가와 사네미", R.raw.sanemi),
-    Character("코쵸우 카나에", R.raw.kanae),
-    Character("우로코다키 사콘지", R.drawable.urokodaki),
-    Character("쿠와지마 지고로", R.drawable.jigoro),
-    Character("츠유리 카나오", R.raw.kanao),
-    Character("시나즈가와 겐야", R.raw.genya),
-    Character("칸자키 아오이", R.raw.aoi),
-    Character("카이가쿠", R.drawable.kaigaku),
-    Character("무라타", R.drawable.murata),
-    Character("우부야시키 카가야", R.raw.kagaya),
-    Character("하가네즈카 호타루", R.drawable.hotaru),
-    Character("키부츠지 무잔", R.raw.muzan),
-    Character("스사마루", R.drawable.susamaru),
-    Character("야하바", R.drawable.susamaru),
-    Character("루이", R.raw.rui),
-    Character("코쿠시보", R.drawable.kokushibou),
-    Character("아카자", R.raw.akaza),
-    Character("도우마", R.drawable.douma),
-    Character("굣코", R.drawable.gyokko),
-    Character("다키", R.raw.daki),
-    Character("한텐구", R.drawable.hantengu),
-    Character("나키메", R.drawable.nakime),
-    Character("쿄우가이", R.drawable.kyogai),
-    Character("엔무", R.raw.enmu),
-    Character("타마요", R.raw.tamayo),
-    Character("유시로", R.raw.yushiro),
-    Character("규타로", R.raw.gyutaro)
+    Character(
+        "카마도 탄지로",
+        "https://mblogthumb-phinf.pstatic.net/MjAxOTExMTBfMjIg/MDAxNTczMzE4MjU4NzE3.FIcHq4HQ7wbvUQdF_Y7s79SdpNQvIfmtIrM3h5vxiKcg.Iq_0iGOyZo_v1UIs15n3JpUPbp0jKFKK9XNQC1M2WZEg.GIF.woojin2352/1573318109.gif?type=w800"
+    ),
+    Character(
+        "카마도 네즈코",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMDAyMjFfMTkg/MDAxNTgyMjc5MDc1ODk4.Rzu_W3J2MkMQY-9-XB-p-dHmEBsB14FszWgo6OcD7HYg.Dbed6kNXtZbUW4dWitnDblhLvAzUPkW1QRrMGacYOM0g.GIF.slpooda/original.gif?type=w800"
+    ),
+    Character(
+        "아가츠마 젠이츠",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMDA4MTlfODUg/MDAxNTk3ODI4NjgwMzc3._m9UOVROh6gl4BkYqayIgT_5QUMDWmhfQL9pDGKKpf0g.U8FCvmjfQElIDckbxNEwzlAmUwuUelkOGk_YN0EUOqIg.GIF.hanihani517/NaverBlog_20200819_181759_01.gif?type=w2"
+    ),
+    Character(
+        "하시바라 이노스케",
+        "https://blog.kakaocdn.net/dn/bONNM2/btqAMPKXJCL/BckOcI2PRGM2oX52HN3rEK/img.gif"
+    ),
+    Character("렌고쿠 쿄쥬로", "http://pds18.egloos.com/pds/201910/21/26/b0365026_5dad2334a3247.gif"),
+    Character(
+        "우즈이 텐겐",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMDA0MjBfMjEz/MDAxNTg3Mzg4NTk2Njkz.klSu2rulNOrKfbiDcaaZT2NE1JZagoswqAQA1Zjyqi4g.uoQJHT5YJVVN20054Q1WqOguOa5XfOMOIL98OsCTT7kg.GIF.jicn9195/1580390150715.gif?type=w800"
+    ),
+    Character(
+        "칸로지 미츠리",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMDAzMDZfMTE5/MDAxNTgzNDk1NzMzNTg0.EzC7OyrrkyKdaschgn7rJm4JPXdp03DDMOdXmRvlzDkg.M7whQ9UmZlgqdCKxLKiM2EkVjemoHBCdBqoFdO-wBUAg.GIF.slpooda/3.gif?type=w800"
+    ),
+    Character(
+        "히메지마 교메이",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMDEwMTRfMTEz/MDAxNjAyNjg1NzI4NTAy.EZNLhMQadHJIb4mY0RiuSF8QFJ4xnVQMnSSz-du9Ft8g.m9e6FtKJf5lIODcQri_eYJK05ocMBYteE_rWil3To8wg.GIF.97_ele_na/IMG_3088.GIF?type=w800"
+    ),
+    Character("토키토 무이치로", "https://www.otakutest.kr/image/test/1627635524965_0"),
+    Character(
+        "이구로 오바나이",
+        "https://mblogthumb-phinf.pstatic.net/MjAxOTEyMTFfMjYw/MDAxNTc2MDcyMTY4MDg4.W9oUI8K30Trn6oQcT6NqJJuK7Brbi-3FWwlr1gn2K00g.ERQ3XCGXXJiOjZK1843x5ULikjLydfawNs7qLELtO8Qg.GIF.jiyun6097/3b48c2ca1418032ba522eab63254dae5.gif?type=w800"
+    ),
+    Character("토미오카 기유", "https://t1.daumcdn.net/cfile/tistory/99C91D335D5F8FE001"),
+    Character(
+        "코쵸우 시노부",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMDAyMjdfMjY1/MDAxNTgyODA0MzMwNzE4.59AS9ZlZDjPJpBCKOKc2js-ONx8IKrz3iqrzTprCE2kg.ZWaR0sro8DxjsBaI03wCfZr29PDcOVYKcMt5S899Jjcg.GIF.slpooda/8215b837c43a9e3f0ad6df4d61e44acd.gif?type=w800"
+    ),
+    Character(
+        "시나즈가와 사네미",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMDAxMThfMjk5/MDAxNTc5MjkyOTA2MjEy.fKxHZ2Z4dIPigdifWudxeHfJhLIqMgtQK4D-Q64VcUcg.clYPzYcTq-ZvsJ944Kef6op_EPsV8b0Aar0Oo05z9xwg.GIF.lovere_/KakaoTalk_20200118_045914546_01.gif?type=w800"
+    ),
+    Character(
+        "코쵸우 카나에",
+        "https://mblogthumb-phinf.pstatic.net/MjAxOTEwMTFfMTIy/MDAxNTcwNzI1NTA4Mzg0.rJZ4j1CDKz3f9Z_x15ibZdiYMKwLi_ApO0GubbTY6IYg.NVs7hQEc6-jmchlC_aM-YUS-5fgj7lWEymgxMFhFSUMg.GIF.inhuckjin85/(%EA%B7%80%EB%A9%B8%EC%9D%98%EC%B9%BC%EB%82%A025%ED%99%94)_%EC%BD%94%EC%B5%B8%EC%9A%B0_%EC%9E%90%EB%A7%A44.gif?type=w800"
+    ),
+    Character(
+        "우로코다키 사콘지",
+        "https://t1.daumcdn.net/cafeattach/1YmK3/3062a76b8bb40afb3bcc83753fd567e8029d4b79"
+    ),
+    Character(
+        "쿠와지마 지고로",
+        "https://blog.kakaocdn.net/dn/C4274/btq4cFZNYFw/QnoqJGmkbIZiKVNNO4tSc0/img.png"
+    ),
+    Character(
+        "츠유리 카나오",
+        "https://mblogthumb-phinf.pstatic.net/MjAxOTExMDZfMTkz/MDAxNTcyOTc1NTUwOTg3.sUYg_4dFd4pjHd94Pax8dGC8-ChL4cNuAo2ohYg0BuMg.7FhKr7TRheE8DzXTuki9YD0UDjOgFF2_KXcLLLV07Dsg.GIF.inhuckjin85/(%EA%B7%80%EB%A9%B8%EC%9D%98%EC%B9%BC%EB%82%A026%ED%99%94)_%EB%91%90%EA%B7%BC%EB%91%90%EA%B7%BC_%EC%B9%B4%EB%82%98%EC%98%A4.gif?type=w800"
+    ),
+    Character(
+        "시나즈가와 겐야",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMTAxMDZfMjkz/MDAxNjA5OTE4ODE3OTY2.EunOztd-2BMBSVeILKadre6x3O3z0MB5ILjye8GskyEg.h1YasGvmw1c5l1gclLYZqOr0KuJGSTGs4inOJbE9-kIg.GIF.dldmstj0944/IMG_1067.GIF?type=w800"
+    ),
+    Character("칸자키 아오이", "https://img.chuing.net/i/QVGVuGJ/a778827-20190908-011714-000.gif"),
+    Character(
+        "카이가쿠",
+        "https://64.media.tumblr.com/894a8ad62edece10053a93eb338e092b/tumblr_pvbqng50Jt1r60zuio1_540.gif"
+    ),
+    Character("무라타", "https://thumbs.gfycat.com/IncompleteScholarlyGrayfox-size_restricted.gif"),
+    Character(
+        "우부야시키 카가야",
+        "https://mblogthumb-phinf.pstatic.net/MjAxOTEyMDZfMTI4/MDAxNTc1NjMyMTI4OTE5.6aWqt1vzisfzT2GyPDtHE0WICcxJqUE7HrwoJIQ9Zncg.izKRmKu-KoLIeg1MrshjEmiKwtoAwubgKJuOnlMrYWAg.GIF.jiyun6097/e7a6039c-2666-4d93-94e6-ddea8b152121-1430737784012.gif?type=w800"
+    ),
+    Character(
+        "하가네즈카 호타루",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMDA0MDNfNDUg/MDAxNTg1OTExMTA4ODQ3.Zyx-fQ5cB7u6gXKBQI6PsdXnAVYY_bg8f0yLCxSfqdIg.PDMZkREJeWDTzdI7pgYWE-RtRJ0OVQubRg7jiULKevIg.GIF.yejinko520/1585911107883.gif?type=w800"
+    ),
+    Character(
+        "키부츠지 무잔",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMDAxMjFfMjU3/MDAxNTc5NjAyMjMwMTky.oDJowd6SEmi57E59D0-9ZeC2RHrLjL9mnYwU_W138Gog.6gI-vqnaH9_s9-_rC-tFQ7wmsdVDXyMIDfMq57zRvvgg.GIF.papdghmoon/1.gif?type=w800"
+    ),
+    Character(
+        "스사마루",
+        "https://mblogthumb-phinf.pstatic.net/MjAxOTA2MDJfMSAg/MDAxNTU5NDA1ODg4MjEw.7cMsun8DSnnxSzTx5AwSvzi2emm2Z-uBsueVQt3PKnUg.K3ONo--9Wj1qtsxO5v8Z0vxr99PoNFeenKGrsd0Vsuog.JPEG.max9607/Ohys-Raws_Kimetsu_no_Yaiba_-_09_(BS11_1280x720_x264_AAC)_0000300423ms.jpg?type=w800"
+    ),
+    Character(
+        "야하바",
+        "https://1.gall-img.com/tdgall/files/attach/images/82/844/188/067/b765e8887e52dda2be17dd944a9acdd5.png"
+    ),
+    Character(
+        "루이",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMDA5MDFfODEg/MDAxNTk4OTI0MjM4MTM3._nroD7iRTJo7Kv4Q4YO0XQgghYMY5pvg_qJFi-e_lgMg.2b7vY0wPS4qL_VUn8JrltGKPVziArxK6zpuCEJMEl8Ug.GIF.daffodil22/1598924235351.GIF?type=w800"
+    ),
+    Character("코쿠시보", "https://pbs.twimg.com/media/Fb0rCVGVUAAUGg8.jpg"),
+    Character(
+        "아카자",
+        "http://file3.instiz.net/data/file3/2021/05/10/9/3/2/93292f07693424a4f22f8f248119ea6a.gif"
+    ),
+    Character(
+        "도우마",
+        "https://1.gall-gif.com/tdgall/files/attach/images/82/015/610/212/8e3d4b1255ddd9bc8f0d74668831ae8e.gif"
+    ),
+    Character(
+        "굣코",
+        "https://image.fmkorea.com/files/attach/new2/20220105/486616/1028630410/4225257483/99b983892094b5c6d2fc3736e15da7d1.png"
+    ),
+    Character("다키", "https://simg.donga.com/ugc/MLBPARK/Board/16/13/39/69/1613396928029.gif"),
+    Character(
+        "한텐구",
+        "https://blog.kakaocdn.net/dn/bR7vjr/btqZIA3VyZQ/NYphTr1yTkKXp2VI3oKCkk/img.jpg"
+    ),
+    Character(
+        "나키메",
+        "https://postfiles.pstatic.net/MjAyMTAyMDRfMTY3/MDAxNjEyMzk2NDQzOTU5.f-SnZb1BA4xlq9cPuhqKcCfmFJqwmH9nLZ53SGEmUnwg.y3hUX3aAtek0JzwdLo5RCUyZy44xI7zHIQz8JHi_xc0g.JPEG.9913065/%EB%82%98%ED%82%A4%EB%A9%94.jpg?type=w773"
+    ),
+    Character(
+        "쿄우가이",
+        "https://post-phinf.pstatic.net/MjAyMTA5MDlfMTI4/MDAxNjMxMTc2NTI3MjY4.w92Go0MCZ5LjoVOjOx4jq2_aFDdnvbscYgmSUqr-Kywg.qfyAv_aaY-i7IOtAJgbtqNwUYSjhV4cVBNcLcsQcgTIg.JPEG/210909_%EA%B7%80%EB%A9%B8%EC%9D%98%EC%B9%BC%EB%82%A0_1%EA%B8%B0_13%ED%99%94_%EC%95%A0%EB%8B%88_19.jpg?type=w1200"
+    ),
+    Character("엔무", "https://i.pinimg.com/originals/93/27/4c/93274c1e773c520d5fa76241fb99ba6b.gif"),
+    Character("타마요", "https://img.dmitory.com/img/201910/2AQ/MD9/2AQMD9K1vyeWAoGiKoyAuk.gif"),
+    Character(
+        "유시로",
+        "https://postfiles.pstatic.net/MjAyMDAzMTRfMTgz/MDAxNTg0MTg3MzAyNTQ1.257OX5PO8UEXyiN9h1RE9G2UjwEebU99tyh8-cjPALAg.0P3nw-bQXs4_E7yQn_9rOZyyjHml4-qtbdeQuPQjNPcg.GIF.slpooda/%EC%9C%A0%EC%8B%9C%EB%A1%9C.gif?type=w773"
+    ),
+    Character(
+        "규타로",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMjAxMTdfNSAg/MDAxNjQyNDE3ODE2OTQ1.ogN_ULdXATUwJj4yd_0zlvfDlUoxFuN4vq9Buzcwmy8g.6RNy2h3uAbq-_M7jxLrxbOMQfI11N7Ry4fD3cgScOY8g.GIF.fjdkeivn/2.gif?type=w800"
+    )
 )

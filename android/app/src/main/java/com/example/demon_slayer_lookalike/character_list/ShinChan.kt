cff581b03a83494783517b9506ee61b20816d5c7
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-04-04 13:33:49 +0900

    짱구 캐릭터 정리

--- android/app/src/main/java/com/example/demon_slayer_lookalike/character_list/ShinChan.kt
+++ android/app/src/main/java/com/example/demon_slayer_lookalike/character_list/ShinChan.kt
@@ -1,27 +1,79 @@
 package com.example.demon_slayer_lookalike.character_list
 
-import com.example.demon_slayer_lookalike.R
-
 val shinChan = arrayOf(
-    Character("짱구", R.id.demonSlayerBtn),
-    Character("신형만", R.id.demonSlayerBtn),
-    Character("봉미선", R.id.demonSlayerBtn),
-    Character("신짱아", R.id.demonSlayerBtn),
-    Character("흰둥이", R.id.demonSlayerBtn),
-    Character("김철수", R.id.demonSlayerBtn),
-    Character("한유리", R.id.demonSlayerBtn),
-    Character("이훈이", R.id.demonSlayerBtn),
-    Character("맹구", R.id.demonSlayerBtn),
-    Character("한수지", R.id.demonSlayerBtn),
-    Character("치타", R.id.demonSlayerBtn),
-    Character("원장선생님", R.id.demonSlayerBtn),
-    Character("채성아", R.id.demonSlayerBtn),
-    Character("나미리", R.id.demonSlayerBtn),
-    Character("차은주", R.id.demonSlayerBtn),
-    Character("철수엄마", R.id.demonSlayerBtn),
-    Character("유리엄마", R.id.demonSlayerBtn),
-    Character("훈이엄마", R.id.demonSlayerBtn),
-    Character("유이슬", R.id.demonSlayerBtn),
-    Character("박광자", R.id.demonSlayerBtn),
-    Character("액션가면", R.id.demonSlayerBtn),
+    Character(
+        "짱구",
+        "https://blog.kakaocdn.net/dn/bD87bf/btrvWHixZRP/kc2HuB51AoJQeelpWhHO7k/img.gif"
+    ),
+    Character(
+        "신형만",
+        "https://mblogthumb-phinf.pstatic.net/20150826_106/snrnsms1104_1440599019899rTGfT_GIF/%25EC%258B%25A0%25ED%2598%2595%25EB%25A7%258C%25EB%25B0%259C%25EB%2583%2584%25EC%2583%2588%25ED%2582%25A5.gif?type=w800"
+    ),
+    Character(
+        "봉미선",
+        "http://file3.instiz.net/data/file3/2018/01/29/f/f/2/ff2c2e0f117fe30466acf9a4d96f2396.gif"
+    ),
+    Character(
+        "신짱아",
+        "https://gif.fmkorea.com/files/attach/new/20170221/486616/82743308/588729634/5b079caab5f32d13cf6c060dddade3b3.gif"
+    ),
+    Character(
+        "흰둥이",
+        "https://blog.kakaocdn.net/dn/tRyZ4/btrK2mxuw2q/rI1o6QkkAPx3RlN6rz1ts1/img.gif"
+    ),
+    Character(
+        "김철수",
+        "https://mblogthumb-phinf.pstatic.net/MjAxODA5MTVfODcg/MDAxNTM3MDE0OTk2MzY5.sx8qvvmnbAcNz3lA2i92L04Er7nGZ_xQW_gl_vmU3nkg.ln-in8wL2dmEJPW4gUghkFA_ttaFZTuKegmELWnuAG8g.GIF.fkal3551/짱구움짤1철수.gif?type=w800"
+    ),
+    Character(
+        "한유리",
+        "https://blog.kakaocdn.net/dn/br6Tvj/btrvS796Iox/k3Lm2SbsjElbwi2ToaEy5k/img.gif"
+    ),
+    Character(
+        "이훈이",
+        "https://mblogthumb-phinf.pstatic.net/MjAxODA5MTVfNTMg/MDAxNTM3MDE0OTk4NjEx.OhaCFTl2tHUgfauqn0C72RGoZFX7O2_fEkNBvb1G83Ig.3_1jakK2t1g_0gJj8buhUZ0SjIJmv1QQDkaa8Tpgux4g.GIF.fkal3551/짱구움짤14훈이.gif?type=w800"
+    ),
+    Character(
+        "맹구",
+        "https://mblogthumb-phinf.pstatic.net/MjAxODA5MTVfMTY3/MDAxNTM3MDE0OTk5NjUy.iqBekYYrDUH8-jz3KzcV5F3Vf2YFk8achjCSG10moTog.B24IjRojDUrqjqlISlBabdmKZEUW7Iu3bHIJR8ySRKIg.GIF.fkal3551/짱구움짤17맹구.gif?type=w800"
+    ),
+    Character(
+        "한수지",
+        "https://i.namu.wiki/i/xGbcB5VRGopQwPNk9Z_5Kj_BZdG2rrdpuyiwQEXeuZIlFQphWt7mfps0lLYguhL5WTpfd7a8GXeX5fL4KvyulA.gif"
+    ),
+    Character(
+        "치타",
+        "https://mblogthumb-phinf.pstatic.net/MjAxOTAyMjBfMjM3/MDAxNTUwNjM4MTA2MTY1.9YmynmTsQr8JFB3nCZPOIYyl60LwtA0TJdlnEM1vRXIg.jUu6JLmCC1ICFeWy3R4Oy6HcJIWjQsTfFJtLpj40ZBEg.JPEG.sinnoske1992/downloadfile-35.jpeg?type=w800"
+    ),
+    Character(
+        "원장선생님",
+        "https://mblogthumb-phinf.pstatic.net/MjAxOTA2MDRfMjM3/MDAxNTU5NjAyNjk0Mzkw.fGayssFM9xtEdfq0IK-CMKe4bVAk-fZsH50DodUdvXMg.tqDn8azkKKcQDc5P0WsA5GTTuHM3vOKtukI4MoiOSpUg.GIF.qkrtpal789/2794b0bb28ed853a621bc9987a1a15455473018421245144610.gif?type=w800"
+    ),
+    Character("채성아", "https://img.jbox.co.kr/UpFiles/Content/Image/1409/shot_l/1409_8.jpg"),
+    Character(
+        "나미리",
+        "https://dispatch.cdnser.be/wp-content/uploads/2017/12/20171228172011_1-176.jpg"
+    ),
+    Character(
+        "차은주",
+        "https://mblogthumb-phinf.pstatic.net/MjAxODA5MTVfMjMy/MDAxNTM3MDE0OTk5NDM3.7qQcd8wT7g6Mq0Pf4GNxcWR9rQ6ZApx7B7hxp_yBpZEg.5nQ0-qGp34KqkTd-lWkalsUANSE9LBDw5EC1-RjzyWog.GIF.fkal3551/짱구움짤10선생님.gif?type=w800"
+    ),
+    Character(
+        "철수엄마",
+        "https://img.extmovie.com/files/attach/images/135/622/901/048/e69ba17f46cdad1a935d7ceb1a3cf808.jpg"
+    ),
+    Character(
+        "유리엄마",
+        "https://scontent-ssn1-1.xx.fbcdn.net/v/t1.6435-9/47191429_488143728428907_3031250968189599744_n.png?stp=dst-png_s180x540&_nc_cat=108&ccb=1-7&_nc_sid=1480c5&_nc_ohc=FftQunhDpzAAX9fKFas&_nc_ht=scontent-ssn1-1.xx&oh=00_AfDKMVp4pMJy4WkUCE71xTblJcEbkIuf98bSd5EZrgTN4g&oe=64530E63"
+    ),
+    Character(
+        "훈이엄마",
+        "https://static.wikia.nocookie.net/shinchan/images/e/ee/%ED%9B%88%EC%9D%B4_%EC%97%84%EB%A7%88.png/revision/latest?cb=20140112124434&path-prefix=ko"
+    ),
+    Character("유이슬", "https://pbs.twimg.com/media/FlLfhPUakAEzSCB?format=jpg&name=large"),
+    Character(
+        "박광자",
+        "https://1.gall-img.com/tdgall/files/attach/images/82/574/921/171/e7d5798d3ad63606ea231307ee8625a8.jpg"
+    ),
+    Character("액션가면", "https://pbs.twimg.com/media/Db3yTZ_VQAI6b4H.jpg")
 )

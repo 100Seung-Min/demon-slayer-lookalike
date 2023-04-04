6c5dc35b68dae78b8081681352fdf1579add2394
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-04-04 14:54:09 +0900

    지브리 캐릭터 정리

---
+++ android/app/src/main/java/com/example/demon_slayer_lookalike/character_list/Ghibri.kt
@@ -0,0 +1,116 @@
+package com.example.demon_slayer_lookalike.character_list
+
+val ghibri = arrayOf(
+    Character(
+        "산",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMDAxMTVfODEg/MDAxNTc5MDYwNjM0MTc4.099JrFdIWAnLfY91fRFudJn-xe7v-8Mr_Fj-VnNQyIcg.190t2-4UgZRvzEygmzIkhOOWjU5wbVYvDJht6bGMkCog.GIF.unfadable_/4647e4396cd6349a27ff242aa92ef51a.gif?type=w800"
+    ),
+    Character(
+        "아시타카",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMDExMzBfMjIz/MDAxNjA2NzE5OTU5NDA3.scHWyYM7-DuRjDFQ9_jqoSsvvLrDnjUMK_VvcDecX3wg.lf7UqkLmm9qqd7gnD_ZNrYG2Z7nLPf8Gt_fF6Y34nEwg.GIF.nodoubtshe/3-1.gif?type=w2"
+    ),
+    Character(
+        "에보시 고젠",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMDA0MDZfNjMg/MDAxNTg2MTUwMTE4NjM4.wUNZqFOwJgQEA7hOpKcOQ-d129fEG9jcMd3_B2DDuJkg.TRsbm7ru7wDzQslxavYeEDd2RII7wGRMWONEZPETdBAg.GIF.eunjiyoon03/%EC%97%90%EB%B3%B4%EC%8B%9C_(1).gif?type=w800"
+    ),
+    Character(
+        "가마 할아범",
+        "https://thumbs.gfycat.com/OrganicCelebratedEmeraldtreeskink-size_restricted.gif"
+    ),
+    Character(
+        "가오나시",
+        "https://mblogthumb-phinf.pstatic.net/MjAxODEwMjlfMjEz/MDAxNTQwNzk5NzMwMjIy.BIxALhIwdlDa4UI-zkTvbKNYvyTT-Zjjq_wM2TR0Z4Ig.dcHblUgozbPe04NAmoX6ZOvJ17i3blZL2jW0JWX0bVwg.GIF.wkqch9/DmhOm1X.gif?type=w800"
+    ),
+    Character(
+        "유바바",
+        "https://mblogthumb-phinf.pstatic.net/20160622_151/saaya1217_1466578005363IpkuA_GIF/%C0%AF%B9%D9%B9%D9.gif?type=w2"
+    ),
+    Character(
+        "하쿠",
+        "https://mblogthumb-phinf.pstatic.net/MjAxNzA2MTBfMTQy/MDAxNDk3MDgwMjM1MTcy.Gzad3v107IaIHdaG7m9HdSsGFQTp6TxksebTfmFFdKQg.KHilcbsewRt1314DS6OO-OHgZdWGS1xaI6AdkuA0sU0g.GIF.sayaoikies777/daum_gallery_photo_20170610113938.gif?type=w2"
+    ),
+    Character(
+        "치히로",
+        "https://mblogthumb-phinf.pstatic.net/MjAxNzA4MTdfMjQ3/MDAxNTAyOTc2OTQ3ODg4.xKjR0Nvz9DgXhQkm6iJgqm5XO_TFUjhVyXj3F2RmMaQg.KtEN7k9mtcuVgxsNnevvg_yH2WKb8P5qaIGgFWYtIBAg.GIF.sinnam88/%EC%84%BC%EA%B3%BC_%EC%B9%98%ED%9E%88%EB%A1%9C%EC%9D%98_%ED%96%89%EB%B0%A9%EB%B6%88%EB%AA%85_%EC%9B%80%EC%A7%A4_%2827%29.gif?type=w800"
+    ),
+    Character(
+        "보우",
+        "https://38.media.tumblr.com/50bb36cff3ac667ec2a6f5273c562117/tumblr_naxqnwSQVY1t89rpeo1_500.gif"
+    ),
+    Character(
+        "린",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMDA0MTBfMjEw/MDAxNTg2NDk3Mjk4Mzc2.8j50stnY00aI73Uacv2G9F8gaGT1sRh2i7TkxHMmzw4g.jNcX9IaxzwSEhmXKu_yImFV8ENqHbQkWXTRyr0TTOLYg.GIF.suribyblossom_drama/original_(11).gif?type=w800"
+    ),
+    Character(
+        "마르클",
+        "https://mblogthumb-phinf.pstatic.net/20141213_265/nightsho_14184233971070jvX5_GIF/%B9%AB%C1%A63.gif?type=w2"
+    ),
+    Character(
+        "설리반",
+        "https://postfiles.pstatic.net/MjAyMDA0MDhfMTkx/MDAxNTg2MzIzNTE4NTQw.WH3rbMy99dQ8xOxZdqFUyljrDI-WhQMjdutpgUbQqeUg.YSo1goLYBqPkZBInMprkold3fFdu7XkpOnR-xBA-jg4g.PNG.crews_official/%ED%95%98%EC%9A%B8%EC%9D%98_%EC%9B%80%EC%A7%81%EC%9D%B4%EB%8A%94_%EC%84%B1_%ED%95%B4%EC%84%9D_(2).png?type=w966"
+    ),
+    Character(
+        "소피",
+        "https://mblogthumb-phinf.pstatic.net/MjAxNzA4MTFfNDUg/MDAxNTAyMzk5MDk0MDQx.gxei__hsTM1tacX7LLR30gHVgsxQCTnp4hMJJFr_3jEg.TK6s6VNb8IHm0GsZ2iX7QJ-Cb3IE30iyTppjjuUYxk4g.GIF.btskook91/tumblr_opuog4ZQAP1uojky7o1_540.gif?type=w2"
+    ),
+    Character(
+        "캘시퍼",
+        "http://file3.instiz.net/data/file3/2018/03/19/8/9/a/89a1a047703541f09ca257f91cdc035b.gif"
+    ),
+    Character(
+        "하울",
+        "https://mblogthumb-phinf.pstatic.net/MjAxNzA4MTFfMTEg/MDAxNTAyMzk4NTcxOTc5.e7vQKFk0tUKYF-hWtTQ9smKLC1MkI9gA7ZPp_-9eQlcg.BtxR2w9OFrn3bZLNXuM-OFkAMfxZhEkjS_fv34r5Hc4g.GIF.btskook91/tumblr_ogg073CsS41ulrwwwo1_540.gif?type=w2"
+    ),
+    Character(
+        "황야의 마녀",
+        "https://mblogthumb-phinf.pstatic.net/MjAxNzAyMDZfMjE1/MDAxNDg2MzYwMjM0NzE0.mt90dJsmBWvxpvT_ySQMxqI050DyCRIfr8C_zrEafMEg.0w5P2_T8ArtQch2XmxqsdGo7d4DDAk2YtRJ6pYLJyKog.PNG.hanbeak0308/2017-02-06_14%3B49%3B31_0.png?type=w2"
+    ),
+    Character(
+        "허수아비",
+        "https://blog.kakaocdn.net/dn/bkEIMc/btqC5pwDiTS/l4WWnHPwGRPaj2fcxPWXyK/img.gif"
+    ),
+    Character("나우시카", "http://i.imgur.com/iDNixLV.gif"),
+    Character(
+        "아스벨",
+        "https://mblogthumb-phinf.pstatic.net/20161005_239/vkti1122_14756282943900pso1_JPEG/movie_image.jpg?type=w2"
+    ),
+    Character("크샤나", "https://t1.daumcdn.net/cfile/tistory/2729853A566007C739"),
+    Character(
+        "도라",
+        "https://68.media.tumblr.com/73abf3c4a604b64b4ed3d5dad1ffeee8/tumblr_inline_oq2kk0UQgt1sebu2v_540.gif"
+    ),
+    Character("파즈", "https://thumbs.gfycat.com/CreativeOddCardinal-size_restricted.gif"),
+    Character("시타", "http://i.imgur.com/5sSw7WQ.gif"),
+    Character(
+        "토토로",
+        "https://steamuserimages-a.akamaihd.net/ugc/3299190295345444513/1470A7C0751981A5A011688B373D812A00915756/?imw=1024&imh=551&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true"
+    ),
+    Character("키키", "http://i.imgur.com/ehRrVaZ.gif"),
+    Character("고양이 버스", "https://thumbs.gfycat.com/AngelicColorlessHornedtoad-size_restricted.gif"),
+    Character("포뇨", "https://i.pinimg.com/originals/e1/9e/ce/e19ece29d57135a0c72c1bf7e619eddc.gif"),
+    Character(
+        "소스케",
+        "https://mblogthumb-phinf.pstatic.net/MjAxNjExMjRfMjcx/MDAxNDc5OTc3MTE1Mzc1.ZPX1J7MSUEthagNebLi3hdT6_qT5V0Q4EcrJSIKgVScg.Dh85fpVwJP5216MGjBMZgUBZ1XwLcdzhDRUYHqM4NF8g.GIF.sinnam88/%EB%B2%BC%EB%9E%91%EC%9C%84%EC%9D%98_%ED%8F%AC%EB%87%A8_%EC%9B%80%EC%A7%A4_%EB%AA%A8%EC%9D%8C_%2822%29.gif?type=w800"
+    ),
+    Character(
+        "후지모토",
+        "https://i.pinimg.com/originals/b9/b7/2d/b9b72d4238a42b9e43e1543c233587e7.gif"
+    ),
+    Character(
+        "그랑 맘마레",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMTA1MDlfOTQg/MDAxNjIwNTY5NjY1Njg2.AoHgAoJO4J-eJY_nxMFc05MO9WwpIHyHFxEmdfmqZiwg.j19i-8THHlkNOXZ4Wx4SFbm7hgxAS_kx8hF397qMNQgg.JPEG.yancu32/%EB%B2%BC%EB%9E%91%EC%9C%84%EC%9D%98_%ED%8F%AC%EB%87%A8_(2).jpg?type=w800"
+    ),
+    Character(
+        "아리에티",
+        "https://mblogthumb-phinf.pstatic.net/20131031_288/ho809_1383225502094J9oID_GIF/anigif.gif?type=w800"
+    ),
+    Character(
+        "쇼우",
+        "https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjNfMjkg/MDAxNDkwMjYwNjU4MDQ1.M1q2BawXEE4pKm02BPq6cAcjPyq7ZQ864yKukJHaCmcg.8LDksn68Zu7GYjOYxOK9Hvs3_kKG9FWFV0GR1pRn6Igg.GIF.the2asy/%EB%A7%88%EB%A3%A8%EB%B0%91%EC%95%84%EB%A6%AC%EC%97%90%ED%8B%B06.gif?type=w800"
+    ),
+    Character(
+        "요시오카 하루",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMDAzMzFfMTgy/MDAxNTg1NjQ1NjIwMDk3.NPlasOjhX4uB-lHqbOGPuojMmeGFW2DS6r1CZnSLIhgg.bPEVhlFKauZXzByLXLIyB_wh-1fY6ala05QGTP53htAg.JPEG.mspot0223/4.jpg?type=w800"
+    ),
+    Character("바론", "https://blog.kakaocdn.net/dn/D3kf4/btq2MOYRJ58/w6cE51625I1dhMzXaomBL0/img.jpg")
+)

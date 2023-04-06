476a30cccab15558b5feac3db9274c86e974234b
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-04-06 11:47:43 +0900

    코난 캐릭터 정리

---
+++ android/app/src/main/java/com/example/demon_slayer_lookalike/character_list/Conan.kt
@@ -0,0 +1,81 @@
+package com.example.demon_slayer_lookalike.character_list
+
+val conan = arrayOf(
+    Character("코난", "https://media.tenor.com/cJ3q4osVwEgAAAAC/detective-detective-conan.gif"),
+    Character(
+        "남도일",
+        "https://mblogthumb-phinf.pstatic.net/MjAxNzAyMTVfMTE2/MDAxNDg3MTUwOTY3MjY2.pkpp8vKFCwmSSND0ND7DgPpIX4gm4YOocST3cr2G2S8g.KArDJRWw23r5aekY09RFZUGvEH0DjhpglDwut_OXibMg.GIF.unesco1128/tumblr_njtauzFaDb1rwf16ho1_400.gif?type=w2"
+    ),
+    Character(
+        "유미란",
+        "https://mblogthumb-phinf.pstatic.net/MjAxNzA2MDFfMzMg/MDAxNDk2MjkxMTg5NzY5.DmMlmPrgq3SAfivCJ5BWAMDNLMRC25iawGm_8VNMpCsg.a_McnNvDp7_HbuLl7_L7srBmFWnzoTBrz4gyJGxLHMwg.GIF.unesco1128/tumblr_o9u7p12Q4K1tmvxxdo1_500.gif?type=w2"
+    ),
+    Character(
+        "유명한",
+        "https://mblogthumb-phinf.pstatic.net/MjAxODAzMDFfMjA1/MDAxNTE5ODg5OTM3NjAx.KYM2knG2sfLy5Tqd3ZTBMz_gPbYYyN-GlRGQuhARKxAg.RLyr0EG7RCAx9rIg2sEhPaaWM-7MlztKBLrzI7q2sKsg.PNG.cup0527/5f3b14123cc17a7fc9a51f1a9fb1c4a6c783be2da7e39ba9d0dc73ae00af3e0214e515bbabf2.png?type=w2"
+    ),
+    Character("홍장미", "http://img.dmitory.com/img/201808/4vW/RLJ/4vWRLJb5GE4UgCI0C6AEgk.gif"),
+    Character(
+        "한아름",
+        "https://mblogthumb-phinf.pstatic.net/20160909_149/smiles115_1473400852114WNAuK_PNG/Conan_287.PNG?type=w2"
+    ),
+    Character(
+        "고뭉치",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMDA5MjBfMjkx/MDAxNjAwNTMxMDg1MTg0.pLXv5LtfcuTWj5Jmmar-5TWr6jA_hRn7pwCQlYZw-lMg.JzTdnNszLdKMMp2xH5OeEnzYKoYERuMtFSq6BAyH0wsg.JPEG.lara46/Genta_Kojima_Profile.jpg?type=w800"
+    ),
+    Character(
+        "박세모",
+        "http://file.instiz.net/data/file/20130831/2/b/a/2bacbc690eae5990d2d30c84f68ceec4"
+    ),
+    Character(
+        "안기준",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMTA5MDJfMjkw/MDAxNjMwNTk0MTAwODgx.lSIQvDyALDDz4KKV07P-_Uz-_iAtgIdh0cCJlxvd2Lkg.BlepgIRTonDU1c8JCqFE6D4QIBYv2XKvTh0VS6xHx5Eg.GIF.cheun0423/IMG_2977.GIF?type=w800"
+    ),
+    Character("정보라", "https://s.gae9.com/trend/931c67d32d3ae492.orig"),
+    Character(
+        "양세라",
+        "https://mblogthumb-phinf.pstatic.net/MjAxODA4MTRfNDYg/MDAxNTM0MjU4NzcyMzY5.-e9qmUlbbYmN91Hxu_Klz23XQGcp-tZpJ6RveJZqCp0g.CkxErmzp0lTFQjUtdCYr-2H6wrn33Q2M1POx3_QTqY8g.GIF.cheun0423/%EC%84%B8%EB%9D%BC.gif?type=w800"
+    ),
+    Character(
+        "고희도",
+        "http://file3.instiz.net/data/cached_img/upload/2020/06/09/23/670cf2d6511ab11e7c758a92d0f45189.gif"
+    ),
+    Character(
+        "하인성",
+        "https://mblogthumb-phinf.pstatic.net/20160922_101/smiles115_1474529534961r9Vuc_GIF/Conan_647.gif?type=w2"
+    ),
+    Character("서가영", "https://t1.daumcdn.net/cfile/tistory/99CD543C5B8968FC1C"),
+    Character(
+        "골롬보",
+        "https://coresos-phinf.pstatic.net/a/318e23/c_64fUd018svc5smjdh8bbpgp_d68sbu.jpg?type=e1280_std"
+    ),
+    Character("신형선", "https://thumbs.gfycat.com/CheapSourAyeaye-size_restricted.gif"),
+    Character(
+        "오지인",
+        "https://mblogthumb-phinf.pstatic.net/MjAyMTA0MjdfMjc1/MDAxNjE5NTMyNTg4MDA1.JwV8gzhrQ6Q3-H1eCKwKvJMT24VVVYbnDy5FzK-CMKMg.Cr0gmQ6G3xDhsxa2aC0XHemUBnGC5YWy3meNd5eL18Ag.JPEG.culturecre/IMG_20210416_162454_037.jpg?type=w800"
+    ),
+    Character(
+        "백동훈",
+        "https://postfiles.pstatic.net/20121117_130/ddydrl7120_1353079744614Ec5Qp_PNG/%BD%C3%B6%F3%C5%E4%B8%AE.png?type=w2"
+    ),
+    Character(
+        "이명수",
+        "https://blog.kakaocdn.net/dn/te2Ti/btrfs6Loli0/ftu28HFlDRVLoPdqA2sX11/img.jpg"
+    ),
+    Character(
+        "진",
+        "https://file3.instiz.net/data/file3/2018/11/27/9/6/f/96fccb6ec466f57deff2182501413982.gif"
+    ),
+    Character(
+        "워커",
+        "https://post-phinf.pstatic.net/MjAxODA4MDhfMTA5/MDAxNTMzNzA5OTExNzY2.Wt0GLqRIdb2BGk9Y2JiFxnAiY611klyt8thnTQBkq-Yg.6WFch0UuJRuGuBTXOmgWAxPFyxqy5RjHWALvhxUO9I4g.PNG/image_8044388881533709857413.png?type=w1200"
+    ),
+    Character(
+        "베르무트",
+        "https://mblogthumb-phinf.pstatic.net/MjAxODAzMDJfMTgg/MDAxNTE5OTc2OTA0NTIx.JfhntyuN4qyyyM2BgYTv0CSaDZHCix_6aOTGRvKg8-4g.2doD_FJh8EYIWxluTYKf1p2PZWXCCBx5c-vEU6VXEsUg.GIF.ysca5837/4.gif?type=w800"
+    ),
+    Character(
+        "키안티",
+        "https://mblogthumb-phinf.pstatic.net/MjAxNjExMDNfMjUg/MDAxNDc4MTU0MDk0NzA4.NwZBWmsJPgpINaCQNH_00rViycHvORirmo8i0Xmt10Ug.Uv1lgJg9ArbjkSTm7UNd92ohdFRAIiLdw2DfPd5w9NEg.PNG.smiles115/Conan_1088.png?type=w2"
+    ),
+)

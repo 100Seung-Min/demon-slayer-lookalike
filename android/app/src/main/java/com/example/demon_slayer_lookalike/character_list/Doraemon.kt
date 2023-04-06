7d77194947de7c07ac2de88d15b9850726bbf129
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-04-06 11:28:48 +0900

    도라에몽 캐릭터 정리

---
+++ android/app/src/main/java/com/example/demon_slayer_lookalike/character_list/Doraemon.kt
@@ -0,0 +1,41 @@
+package com.example.demon_slayer_lookalike.character_list
+
+val doraemon = arrayOf(
+    Character(
+        "도라에몽",
+        "https://mblogthumb-phinf.pstatic.net/20150505_41/kimjieun2001_1430753013150lfwHi_GIF/%BF%C0%B4%CF%C2%BB.gif?type=w2"
+    ),
+    Character(
+        "노진구",
+        "https://post-phinf.pstatic.net/MjAyMDA4MjVfMjAy/MDAxNTk4MzMzOTE2NDkx.Mgf8uOZqxsddHR5qdFD6_1muIxCtgbuN4eLPVd4wk0Yg.SoKgT1ON63_JIC8RZG4ajgzJXvYi0AIM3W6Q6d28JaYg.PNG/20200825_141116.png?type=w1200"
+    ),
+    Character(
+        "노석구",
+        "https://blog.kakaocdn.net/dn/bONOFP/btqBXFNYHxd/ofiSO2xY6cPsNp3xZ44sY1/img.jpg"
+    ),
+    Character(
+        "만퉁순",
+        "https://lh3.googleusercontent.com/-ofFHiLRAVoM/Wu3e8IXmDsI/AAAAAAACNYY/jg73VMJlZW0EZuGcWl0sJ-MBYMMEA1XkQCHMYCw/s0/a52a2c328fdb3dc8e7e42922dfdb072f74cea725.jpg"
+    ),
+    Character(
+        "도라미",
+        "https://scontent-ssn1-1.xx.fbcdn.net/v/t1.6435-9/103632582_1947085235592958_1801137134727595004_n.jpg?stp=cp0_dst-jpg_e15_fr_q65&_nc_cat=100&ccb=1-7&_nc_sid=110474&_nc_ohc=kN87FGr-oo8AX_jYk5c&_nc_ht=scontent-ssn1-1.xx&oh=00_AfCPQjwpFe8ym_to7TaN_C6AghaUCW60ua61jnx7Fb1bfQ&oe=64559E0F"
+    ),
+    Character(
+        "신이슬",
+        "https://mblogthumb-phinf.pstatic.net/20160920_273/xoghks1303_1474374989678itvww_JPEG/%BB%E7%BE%C7.JPG?type=w2"
+    ),
+    Character(
+        "왕비실",
+        "https://obj.the1.wiki/thecloud/temp/ec9995ebb984ec8ba431393837eb85842036ec9b94203236ec9dbc2e6a7067.jpg"
+    ),
+    Character("만퉁퉁", "https://kkukowiki.kr/images/e/e9/%EB%A7%8C%ED%89%81%ED%89%81.pngㅂ"),
+    Character(
+        "박영민",
+        "https://1.bp.blogspot.com/-nv4JQIjNszs/V1v4ZaNktZI/AAAAAAAADa8/ZLxcVgvAWMQIhfmTanN2yOM7rYFF2OcJACLcB/s1600/1%2B%25281%2529.jpg"
+    ),
+    Character(
+        "오진숙",
+        "https://mblogthumb-phinf.pstatic.net/20150428_8/ojsdream_1430221391937l2EUC_PNG/%BF%C0%C1%F8%BC%F7150410.png?type=w2"
+    )
+)

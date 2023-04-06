5ceda9cbbe6ce4cd0fb848844847e1180978c5ba
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-04-06 17:50:41 +0900

    권한 물었는지 확인하는 함수 작성

--- android/app/src/main/java/com/example/demon_slayer_lookalike/utils/PermissionUtils.kt
+++ android/app/src/main/java/com/example/demon_slayer_lookalike/utils/PermissionUtils.kt
@@ -1,5 +1,6 @@
 package com.example.demon_slayer_lookalike.utils
 
+import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
@@ -13,6 +14,15 @@ fun checkPermission(context: Context, vararg permissionList: String): Boolean {
                 permission
             ) != PackageManager.PERMISSION_GRANTED
         ) {
+            return false
+        }
+    }
+    return true
+}
+
+fun isLastPermissionRequest(activity: Activity, vararg permissionList: String): Boolean {
+    permissionList.forEach { permission ->
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
             return false
         }
     }

e00af30b550ddb004a442fe4be2cd65cb2f81f6b
백승민 <bsmin0417@naver.com>
2023-05-04 11:40:09 +0900

    퀴즈 시작 화면 구현

---
+++ android/app/src/main/java/com/example/demon_slayer_lookalike/view/component/quiz/QuizStartFragment.kt
@@ -0,0 +1,25 @@
+package com.example.demon_slayer_lookalike.view.component.quiz
+
+import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.demon_slayer_lookalike.R
+import com.example.demon_slayer_lookalike.databinding.FragmentQuizStartBinding
+import com.example.demon_slayer_lookalike.view.base.BaseFragment
+import com.example.demon_slayer_lookalike.viewmodel.QuizViewModel
+
+class QuizStartFragment: BaseFragment<FragmentQuizStartBinding>(R.layout.fragment_quiz_start) {
+    private val quizViewModel by activityViewModels<QuizViewModel>()
+
+    override fun createView() {
+        binding.quizStartFragment = this
+        quizViewModel.shuffleQuizList()
+    }
+
+    fun onClick(view: View) {
+        when(view) {
+            binding.backBtn -> findNavController().popBackStack()
+            binding.startBtn -> findNavController().navigate(R.id.action_quizStartFragment_to_quizFragment)
+        }
+    }
+}
